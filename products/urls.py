b2dcccaf80ae9f1d1002c5ab8654aa969f23e4a4
Chosum Tashi <samdup@Chosums-MacBook-Air.local>
2019-09-01 14:10:07 -0400

    Add new methods getcard

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('new', views.new),
-    path('error', views.error)
+    path('error', views.error),
+    path('getcard', views.getcard),
+    path('getcard0', views.getcard0),
+    path('getcard1', views.getcard1),
+    path('getcard2', views.getcard2),
+    path('getcard3', views.getcard3),
+    path('getcard4', views.getcard4)
+
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 
 productname = 'Yu-gi-oh cards'
+yugioh_cards = ('dark magician', 'blue eyes WD', 'man eater bug', 'kuriboh', 'toon BEWD')
 
 
 def index(request):
@@ -16,3 +17,24 @@ def error(request):
 def new(request):
     return HttpResponse("New " + productname + " has arrived")
 
+def getcard(request):
+    return HttpResponse(yugioh_cards)
+
+def getcard0(request):
+    return HttpResponse(yugioh_cards[0])
+
+
+def getcard1(request):
+    return HttpResponse(yugioh_cards[1])
+
+
+def getcard2(request):
+    return HttpResponse(yugioh_cards[2])
+
+
+def getcard3(request):
+    return HttpResponse(yugioh_cards[3])
+
+
+def getcard4(request):
+    return HttpResponse(yugioh_cards[4])
