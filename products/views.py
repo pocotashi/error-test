76322871089e41e0b7f3cba6def1ead5d25a34ae
Chosum Tashi <samdup@Chosums-MacBook-Air.local>
2019-08-31 21:32:49 -0400

    change old error message to sold out

--- products/views.py
+++ products/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 productname = 'Yu-gi-oh cards'
 def index(request):
     return HttpResponse('hello world')
-def error(request):
-    return HttpResponse('there is an error with' + productname)
+def soldout(request):
+    return HttpResponse(productname + " is sold out")
